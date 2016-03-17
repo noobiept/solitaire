09092cc8ea403365ef8f74eb534dee343155907b
Pedro Ferreira <plferreira4@gmail.com>
2016-03-17 14:35:20 +0000

    - start reworking the game to be able to play different kinds of solitaire games (not just 'gold mine')     - add a 'gold_mine.cs', where the 'gold mine' game related code will be     - add a 'freecell.cs', where the 'freecell' game code will be (etc)     - move the 'gold mine' code from 'main_window.xaml.cs' to 'gold_mine.cs' - add a menu entry where you'll select the game to play

---
+++ freecell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldMine
+    {
+    class FreeCell : Solitaire
+        {
+        public FreeCell()
+            {
+
+            }
+
+
+        public void restart()
+            {
+
+            }
+
+
+        public void positionResizeElements()
+            {
+
+            }
+
+
+        public void end()
+            {
+
+            }
+        }
+    }
